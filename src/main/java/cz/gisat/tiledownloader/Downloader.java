bbb88913e4601a2bef30066b5098ecddecea0a8d
Martin Babič <martin.babic@gisat.cz>
2016-03-09 20:28:31 +0100

    Download map tiles from remote server.

--- src/main/java/cz/gisat/tiledownloader/Downloader.java
+++ src/main/java/cz/gisat/tiledownloader/Downloader.java
@@ -3,29 +3,66 @@ package cz.gisat.tiledownloader;
 import cz.gisat.tiledownloader.objects.LatLon;
 import cz.gisat.tiledownloader.objects.Tile;
 
+import java.io.*;
+import java.net.URL;
+
 public class Downloader {
     private Tile tileMin;
     private Tile tileMax;
     private int zoom;
 
-    public Downloader( Tile tileMin, Tile tileMax ){
+    public Downloader( Tile tileMin, Tile tileMax ) {
         this.tileMin = tileMin;
         this.tileMax = tileMax;
         this.zoom = this.tileMin.getZoom();
     }
 
-    public Downloader( LatLon latLonMin, LatLon latLonMax, int zoom ){
-        new Downloader( latLonMin.getTile( zoom ), latLonMax.getTile( zoom )  );
+    public Downloader( LatLon latLonMin, LatLon latLonMax, int zoom ) {
+        new Downloader( latLonMin.getTile( zoom ), latLonMax.getTile( zoom ) );
     }
 
-    public void executeDownload(){
+    public void executeDownload() {
         int tCount = 0;
-        for( int x = tileMin.getX() ; x < tileMax.getX() ; x++  ){
-            for( int y = tileMin.getY() ; y > tileMax.getY() ; y--  ){
+        for ( int x = tileMin.getX() ; x <= tileMax.getX() ; x++ ) {
+            for ( int y = tileMin.getY() ; y >= tileMax.getY() ; y-- ) {
                 tCount++;
-                System.out.println( "http://tile.openstreetmap.org/" + zoom + "/" + x + "/" + y + ".png" );
+                String filePath = zoom + "/" + x + "/" + y + ".png";
+                String url = "http://tile.openstreetmap.org/" + filePath;
+
+                System.out.print( "Downloading... " + url );
+                try {
+                    this.saveImage( url, filePath );
+                }
+                catch ( IOException e ) {
+                    e.printStackTrace();
+                }
             }
         }
-        System.out.println( "TileCount:" +tCount );
+        System.out.println( "TileCount:" + tCount );
+    }
+
+    private void saveImage( String imageUrl, String destinationFile ) throws IOException {
+        File imgFile = new File( "map/" + destinationFile );
+        if ( imgFile.exists() ) {
+            System.out.println( "   -> EXISTS!" );
+            return;
+        }
+        imgFile.getParentFile().mkdirs();
+        imgFile.createNewFile();
+        URL url = new URL( imageUrl );
+        InputStream is = url.openStream();
+        OutputStream os = new FileOutputStream( imgFile );
+
+        byte[] b = new byte[ 2048 ];
+        int length;
+
+        while ( ( length = is.read( b ) ) != -1 ) {
+            os.write( b, 0, length );
+        }
+
+        is.close();
+        os.close();
+
+        System.out.println( "   -> DONE!" );
     }
 }
--- src/main/java/cz/gisat/tiledownloader/Main.java
+++ src/main/java/cz/gisat/tiledownloader/Main.java
@@ -5,18 +5,17 @@ import cz.gisat.tiledownloader.objects.Tile;
 
 public class Main {
     public static void main( String[] args ) {
-        int zoom = 10;
+        int zoom = 12;
 
         LatLon latLonMin = new LatLon( 48.916646, 12.216218 );
         LatLon latLonMax = new LatLon( 50.883366, 18.591652 );
 
-        Tile tileMin = latLonMin.getTile( zoom );
-        Tile tileMax = latLonMax.getTile( zoom );
+        for( int z = 0 ; z <= zoom ; z++ ){
+            Tile tileMin = latLonMin.getTile( z );
+            Tile tileMax = latLonMax.getTile( z );
 
-        System.out.println( tileMin.getX() + " - " + tileMin.getY() );
-        System.out.println( tileMax.getX() + " - " + tileMax.getY() );
-
-        Downloader downloader = new Downloader( tileMin, tileMax );
-        downloader.executeDownload();
+            Downloader downloader = new Downloader( tileMin, tileMax );
+            downloader.executeDownload();
+        }
     }
 }
